58651e1d48054167f09885ed9a01f8c132474309
vaibhav-kesarwani14 <vaibhav.kesarwani@cogoport.com>
2023-02-07 23:49:57 +0530

    Changes

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ async def get_user_posts(category_name : str):
 @app.get("/articles/")
 def get_articles():
     articles = Article.select()
-    return [{"title": article.title ,"description": article.short_description} for  article in articles]
+    return [{"id": article.id, "title": article.title ,"description": article.short_description} for  article in articles]
     # return [model_to_dict(article) for article in articles]
 
 #Display Article By Id
